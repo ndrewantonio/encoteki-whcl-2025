596c61471a63d4ddf9bb3a78e7aba96298c773c7
ndrewantonio <ndrewantonio@outlook.com>
2025-07-26 23:18:32 +0700

    update code

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,29 +1,11 @@
-import { useState } from "react";
-import { encoteki_whcl_2025_backend } from "declarations/encoteki-whcl-2025-backend";
+import Mint from "./components/mint.jsx";
+
 
 function App() {
-  const [greeting, setGreeting] = useState("");
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    encoteki_whcl_2025_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
-
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me bro!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <img src="/logo.png" alt="logo" />
+      <Mint />
     </main>
   );
 }
